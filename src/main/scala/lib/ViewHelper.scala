cc722761f44c90136eb8a783b60e3e0caa4af367
BlackPrincess <black.princess.w@gmail.com>
2014-06-02 20:59:03 +0900

    Added options helper

--- src/main/scala/lib/ViewHelper.scala
+++ src/main/scala/lib/ViewHelper.scala
@@ -1,11 +1,13 @@
 package lib
 
+import org.fusesource.scalate.support.RenderHelper
+
 object ViewHelper {
   def options(data: Map[String, String], selected : String, attr : String*) : String = {
     val attributeString = attr.foldLeft("") {_ + " " + _}
     (for { (k, v) <- data } yield
-      s"<option value='$k' ${if (selected == k) "selected='selected'" else ""} $attributeString>$v</option>"
-      ).foldLeft("") { _ + _ }
+      s"""<option value="${RenderHelper.sanitize(k)}"${if (selected == k) """ selected="selected"""" else ""}$attributeString>${RenderHelper.sanitize(v)}</option>"""
+      ).reduceLeftOption{ _ + "\n" + _ }.getOrElse("")
   }
 
   def newlineToBr(s: String) : String = {
--- src/test/scala/lib/ViewHelperSpec.scala
+++ src/test/scala/lib/ViewHelperSpec.scala
@@ -1,17 +1,29 @@
 package lib
 
-package integrationtest
-
 import org.scalatra.test.scalatest._
 import skinny.test.SkinnyTestSupport
-import _root_.lib.ViewHelper
 
 class ViewHelperSpec extends ScalatraFlatSpec with SkinnyTestSupport {
-  it should "options" in {
+  it should "line-break to <br>" in {
     ViewHelper.newlineToBr(
       """one
         |two
         |three""".stripMargin) should be("one<br>two<br>three")
+  }
+
+  it should "put option tags" in {
+    ViewHelper.options(Map(
+      "value1" -> "key1",
+      "value2" -> "key2",
+      "value3" -> "key3"
+    ), "value2") should be(
+      """<option value="value1">key1</option>
+        |<option value="value2" selected="selected">key2</option>
+        |<option value="value3">key3</option>""".stripMargin)
+  }
+
+  it should "escape" in {
+    ViewHelper.options(Map("<" -> ">"), "") should be("""<option value="&lt;">&gt;</option>""".stripMargin)
   }
 
 }
