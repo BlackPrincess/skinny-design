946467bc940e7da50df3d0653367f2fc85da4abe
BlackPrincess <black.princess.w@gmail.com>
2014-06-10 21:27:07 +0900

    Added scaml examples

--- src/main/scala/controller/Controllers.scala
+++ src/main/scala/controller/Controllers.scala
@@ -12,6 +12,7 @@ object Controllers {
 
   object root extends RootController with Routes {
     val indexUrl = get("/?")(index).as('index)
+    val scamlUrl = get("/scaml/?")(scaml).as('scaml)
   }
 }
 
--- src/main/scala/controller/RootController.scala
+++ src/main/scala/controller/RootController.scala
@@ -7,4 +7,8 @@ class RootController extends ApplicationController {
   def index = {
     render("/root/index")
   }
+
+  def scaml = {
+    render("/root/scaml")
+  }
 }
